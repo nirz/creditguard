3ba3158e97258d034792709285014d11758e0977
תומר עופר < >
2016-01-05 16:46:25 +0200

    first commit

---
+++ src/CreditGuard.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomer
+ * Date: 05/01/2016
+ * Time: 11:56
+ */
+
+namespace TomerOfer\CreditGuard;
+
+
+class CreditGuard
+{
+
+}
---
+++ tests/CreditGuardTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomer
+ * Date: 05/01/2016
+ * Time: 12:06
+ */
