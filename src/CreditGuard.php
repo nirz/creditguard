86196378671679e5209203b22c8ee25093695c54
Tomer Ofer <tomerofer@gmail.com>
2016-01-05 17:55:18 +0200

    test

--- src/CreditGuard.php
+++ src/CreditGuard.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: tomer
+ * User: tomer ofer
  * Date: 05/01/2016
  * Time: 11:56
  */
@@ -11,5 +11,270 @@ namespace TomerOfer\CreditGuard;
 
 class CreditGuard
 {
+    private $user_name;
+    private $password;
+    private $terminal;
+    private $mid;
+    private $gateway;
+    private $language; // Eng, Heb
+    private $currency; // ILS, USD
+    private $successUrl = "";
+    private $errorUrl = "";
+    private $cancelUrl = "";
+    private $creditType = "RegularCredit"; //RegularCredit, Payments, IsraCredit, SpecialCredit, SpecialAlpha, PaymentsClub
+    private $uniqueId = "";
+    private $mpiValidation = "Normal"; //Normal, Token, Verify, AutoComm, AutoCommHold, CardNo
 
+    /**
+     * CreditGuard constructor.
+     * @param String $user_name
+     * @param String $password
+     * @param String $terminal
+     * @param String $mid
+     * @param String $gateway
+     * @param String $language (Eng|Heb)
+     * @param String $currency (ILS|USD)
+     *
+     */
+    public function __construct($user_name = "israeli",$password = "I!fr43s!34",$terminal = "0962832",$mid = "938",$gateway = "https://cguat2.creditguard.co.il/xpo/Relay",$language = "Eng",$currency = "ILS")
+    {
+        $this->user_name = $user_name;
+        $this->password = $password;
+        $this->terminal = $terminal;
+        $this->mid = $mid;
+        $this->gateway = $gateway;
+        $this->language = $language;
+        $this->currency = $currency;
+    }
+
+    /**
+     * @param string $dataXml
+     * @return array
+     */
+    private function makeRequest($dataXml = ""){
+        $result = ["result" => ""];
+        $request_string = 'user='.$this->user_name.'&password='.$this->password.'&int_in='.$dataXml;
+
+        $CR = curl_init();
+        curl_setopt($CR, CURLOPT_URL, $this->gateway);
+        curl_setopt($CR, CURLOPT_POST, 1);
+        curl_setopt($CR, CURLOPT_FAILONERROR, true);
+        curl_setopt($CR, CURLOPT_POSTFIELDS, $request_string);
+        curl_setopt($CR, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($CR, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($CR, CURLOPT_FAILONERROR,true);
+        $curl_result = curl_exec( $CR );
+        $error = curl_error ( $CR );
+        if( !empty( $error ) ){
+            $result["message"] = $error;
+        }else{
+            if(stripos($curl_result,'HEB') !== false){
+                $curl_result = iconv("utf-8", "iso-8859-8", $curl_result);
+            }
+            $responseXml = simplexml_load_string($curl_result);
+            if($responseXml !== false){
+                if(isset($responseXml->response->result)){
+                    if($responseXml->response->result == "000"){
+                        $result["result"] = (string)$responseXml->response->result;
+                        if(isset($responseXml->response->doDeal->mpiHostedPageUrl)){
+                            $result["url"] = (string)$responseXml->response->doDeal->mpiHostedPageUrl;
+                        }
+                        if(isset($responseXml->response->doDeal->token)){
+                            $result["token"] = (string)$responseXml->response->doDeal->token;
+                        }
+                        if(isset($responseXml->response->doDeal->token)){
+                            $result["token"] = (string)$responseXml->response->doDeal->token;
+                        }
+                        if(isset($responseXml->response->doDeal->uniqueid)){
+                            $result["uniqueid"] = (string)$responseXml->response->doDeal->uniqueid;
+                        }
+                        if(isset($responseXml->response->doDeal->email)){
+                            $result["email"] = (string)$responseXml->response->doDeal->email;
+                        }
+                    }else{
+                        $result["result"] = (string)$responseXml->response->result;
+                        $result["message"] = (string)$responseXml->response->userMessage;
+                        $result["additionalInfo"] = (string)$responseXml->response->additionalInfo;
+                    }
+                }
+            }
+        }
+        curl_close( $CR );
+        return $result;
+    }
+
+    /**
+     * @param String $uniqueId
+     * @param Float $total
+     * @param int $payments
+     * @return array
+     */
+    public function getRedirectUrl($uniqueId = "", $total = 1.0, $payments = 0){
+        if(empty($uniqueId)){
+            $uniqueId = uniqid();
+        }
+        if(is_numeric($payments) && $payments >= 1 || $payments <= 36){
+            $this->creditType = "Payments";
+        }
+
+        // total need to be in Agorot (or cents in USD or EUR)
+        $total = $total * 100;
+
+        // build the xml for the request
+        $dataXml = <<<XML
+<ashrait>
+    <request>
+        <command>doDeal</command>
+        <requestId/>
+        <version>1001</version>
+        <language>{$this->language}</language>
+        <doDeal>
+            <successUrl>{$this->successUrl}</successUrl>
+            <errorUrl>{$this->errorUrl}</errorUrl>
+            <cancelUrl>{$this->errorUrl}</cancelUrl>
+            <terminalNumber>{$this->terminal}</terminalNumber>
+            <cardNo>CGMPI</cardNo>
+            <creditType>{$this->creditType}</creditType>
+            <currency>{$this->currency}</currency>
+            <transactionCode>Phone</transactionCode>
+            <transactionType>Debit</transactionType>
+            <cardId></cardId>
+            <total>{$total}</total>
+            <validation>TxnSetup</validation>
+            <user></user>
+            <mainTerminalNumber></mainTerminalNumber>
+            <authNumber></authNumber>
+            <numberOfPayments>{$payments}</numberOfPayments>
+            <firstPayment></firstPayment>
+            <periodicalPayment></periodicalPayment>
+            <dealerNumber></dealerNumber>
+            <mid>{$this->mid}</mid>
+            <uniqueid>$uniqueId</uniqueid>
+            <mpiValidation>Normal</mpiValidation>
+            <description></description>
+            <email></email>
+            <clientIP></clientIP>
+            <saleDetailsMAC></saleDetailsMAC>
+            <customerData>
+                <userData1/>
+                <userData2/>
+                <userData3/>
+                <userData4/>
+                <userData5/>
+                <userData6/>
+                <userData7/>
+                <userData8/>
+                <userData9/>
+                <userData10/>
+            </customerData>
+        </doDeal>
+    </request>
+</ashrait>
+XML;
+        // build the request string with the username, password and the xml
+        return $this->makeRequest($dataXml);
+    }
+
+    /**
+     * @return bool
+     */
+    public function credentialsIsValid(){
+        $response = $this->getRedirectUrl();
+        if(empty($response["result"])){
+            return false;
+        }else{
+            if($response["result"] == "405"){
+                return false;
+            }
+            return true;
+        }
+    }
+
+    /**
+     * @param string $user_name
+     */
+    public function setUserName($user_name)
+    {
+        $this->user_name = $user_name;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param string $terminal
+     */
+    public function setTerminal($terminal)
+    {
+        $this->terminal = $terminal;
+    }
+
+    /**
+     * @param string $mid
+     */
+    public function setMid($mid)
+    {
+        $this->mid = $mid;
+    }
+
+    /**
+     * @param string $gateway
+     */
+    public function setGateway($gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    /**
+     * @param string $language
+     */
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+    }
+
+    /**
+     * @param string $currency
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+    }
+
+    /**
+     * @param string $successUrl
+     */
+    public function setSuccessUrl($successUrl)
+    {
+        $this->successUrl = $successUrl;
+    }
+
+    /**
+     * @param string $errorUrl
+     */
+    public function setErrorUrl($errorUrl)
+    {
+        $this->errorUrl = $errorUrl;
+    }
+
+    /**
+     * @param string $cancelUrl
+     */
+    public function setCancelUrl($cancelUrl)
+    {
+        $this->cancelUrl = $cancelUrl;
+    }
+
+    /**
+     * @param string $uniqueId
+     */
+    public function setUniqueId($uniqueId)
+    {
+        $this->uniqueId = $uniqueId;
+    }
 }
--- tests/CreditGuardTest.php
+++ tests/CreditGuardTest.php
@@ -1,7 +1,41 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: tomer
+ * User: tomer ofer
  * Date: 05/01/2016
  * Time: 12:06
  */
+
+$autoloader = require '../vendor/autoload.php';
+$autoloader->add('App',__DIR__.'/../src/');
+
+use TomerOfer\CreditGuard\CreditGuard;
+
+$creditGuard = new CreditGuard();
+
+$creditGuard->setLanguage("Heb"); // default: Eng
+$creditGuard->setSuccessUrl("https://your-domain.com/transactionSuccess");
+$creditGuard->setErrorUrl("https://your-domain.com/transactionError");
+$creditGuard->setCancelUrl("https://your-domain.com/transactionFailed");
+
+// check if user credentials is valid
+// $creditGuard->validCredentials()
+
+// get the url to redirect the user
+$response = $creditGuard->getRedirectUrl(uniqid(),200,2);
+/*
+ * Success response example:
+Array
+(
+    "result" => 000,
+    "url" => "https://cgmpiuat.creditguard.co.il//CGMPI_Server/PerformTransaction?txId=6ac533db-6b9d-4aa1-bf7a-465e021c324c",
+    "token" => "6ac533db-6b9d-4aa1-bf7a-465e021c324c",
+    "uniqueid" => "568bd169b7907",
+    "email" => ""
+);
+
+* Save those token and uniqid in your database and wen the user coming back to "successUrl" collect the data from CreditGuard
+*/
+
+
+//print_r($creditGuard);
